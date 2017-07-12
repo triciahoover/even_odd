cfcc2db7bff7367a32c39e1981a9d87a63b90a1c
triciahoover <tricia.l.hoover@gmail.com>
2017-07-12 14:15:40 -0400

    error

--- even_odd.rb
+++
@@ -1,8 +0,0 @@
-require "minitest/autorun"
-
-class TestMMKata < Minitest::Test
-
-	def test_new_test_example
-		assert_equal(1,1)
-	end
-end	
--- even_odd_test.rb
+++ even_odd_test.rb
@@ -1,8 +1,13 @@
 require "minitest/autorun"
+require_relative "even_odd.rb"
 
-class TestMMKata < Minitest::Test
+class TestEvenOdd < Minitest::Test
 
-	def test_new_test_example
+	def test_assert_that_1_equals_1
 		assert_equal(1,1)
+	end
+
+	def test_input_1_returns_odd
+		assert_equal("odd",evenodd(1))
 	end
 end	
